853a8201a26493b3de44445f6ad1d998d4a73a5a
Paul Gigioli <paul.gigioli@gmail.com>
2020-05-20 23:06:23 +0000

    architecture experiments

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo yum install -y graphviz-devel.x86_64 htop
-pip install pygraphviz --user
+source activate pytorch_p36
+pip install pygraphviz sklearn-hierarchical-classification --user
